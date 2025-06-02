0a43bc3cc0de56bd81b9ae713a12663c13d6b8a4
anselbrandt <ansel@anselbrandt.com>
2025-06-02 03:58:14 -0400

    Update main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/Footer";
 import { TextFade } from "@/components/Textfade";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,6 +9,65 @@ export default function Home() {
         <TextFade direction="up">
           <h1 className="text-8xl font-bold font-serif">ansel brandt</h1>
         </TextFade>
+        <div className="text-xl font-semibold">Some things I've built:</div>
+        {}
+        <div className="flex flex-row space-x-2">
+          <Link
+            className="hover:bg-sky-700 hover:text-neutral-50 transition"
+            href="https://mappingmtl.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            mapping mtl
+          </Link>
+          <div>|</div>
+          <Link
+            className="hover:bg-sky-700 hover:text-neutral-50 transition"
+            href="https://github.com/anselbrandt/mappingmtl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            code
+          </Link>
+        </div>
+        <div className="flex flex-row space-x-2">
+          <Link
+            className="hover:bg-sky-700 hover:text-neutral-50 transition"
+            href="https://okapirank.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Okapi Rank
+          </Link>
+          <div>|</div>
+          <Link
+            className="hover:bg-sky-700 hover:text-neutral-50 transition"
+            href="https://github.com/anselbrandt/okapi-rank"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            code
+          </Link>
+        </div>
+        <div className="flex flex-row space-x-2">
+          <Link
+            className="hover:bg-sky-700 hover:text-neutral-50 transition"
+            href="/side-stacker"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Side-Stacker
+          </Link>
+          <div>|</div>
+          <Link
+            className="hover:bg-sky-700 hover:text-neutral-50 transition"
+            href="https://github.com/anselbrandt/side-stacker"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            code
+          </Link>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <Footer />
